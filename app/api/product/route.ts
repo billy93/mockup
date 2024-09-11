0fae81333fe72e82010922b422744dabe04987bc
Andreas Billy Sutandi <andreas.sutandi@atibusinessgroup.com>
2024-09-11 18:10:29 +0700

    connect shopify

--- app/api/product/route.ts
+++ app/api/product/route.ts
@@ -6,6 +6,9 @@ export async function POST(request : any, response: any) {
     const body = await request.json();
     const { url,title } = body;
     console.log("URL : ", url)
+
+    const apiKey = process.env.SHOPIFY_API_KEY || "";
+
   try {
     const product ={
         "title": title,
@@ -37,7 +40,7 @@ export async function POST(request : any, response: any) {
     const res :any = await fetch('https://746fd6-78.myshopify.com/admin/api/2024-07/products.json', {
         method: 'POST',
         headers: {
-            'X-Shopify-Access-Token': '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', // Use environment variable for access token
+            'X-Shopify-Access-Token': apiKey, // Use environment variable for access token
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({ product }),
